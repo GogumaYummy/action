7c9e4bc2bb2983e2177dd49d1ef6913d879142a6
GogumaYummy <sora1211x@gmail.com>
2023-01-07 19:33:59 +0900

    sadf

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+express()
+  .get('/game', (req, res) => {
+    res.send('1');
+  })
+  .listen(3000, () => {
+    console.log('listening on 3000 port');
+  });
